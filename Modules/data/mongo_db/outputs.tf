63f8b03d41447b631d1a449195b8ece74cc9132f
asliAdarsh <adarshjaiswal9966@gmail.com>
2026-05-23 23:12:42 +0530

    infra: fix root output pointer to consume child module database urn

--- Modules/data/mongo_db/outputs.tf
+++ Modules/data/mongo_db/outputs.tf
@@ -1,8 +1,8 @@
 output "database_urn" {
-  value       = digitalocean_database_cluster.mongodb_cluster.urn
-  description = "The uniform resource name for the MongoDB cluster"
+  # Points to module.MODULE_BLOCK_NAME.CHILD_OUTPUT_NAME
+  value       = module.fintrack_mongodb.database_urn
+  description = "The uniform resource name of the database cluster forwarded from the mongo_db module"
 }
-
 output "database_id" {
   value       = digitalocean_database_cluster.mongodb_cluster.id
   description = "The ID of the MongoDB cluster"
