8d6471951fc24d5d440a6eb3ff7925c91e2402da
asliAdarsh <adarshjaiswal9966@gmail.com>
2026-05-23 22:54:06 +0530

    style: run terraform fmt to fix data.tf alignment constraints

--- Workload/Spokes/fintrack/network/data.tf
+++ Workload/Spokes/fintrack/network/data.tf
@@ -2,12 +2,12 @@ data "terraform_remote_state" "core_network" {
   backend = "s3"
   config = {
     # 1. Fixed: Added https:// prefix to prevent API transport handshake errors
-    endpoint                    = "https://sgp1.digitaloceanspaces.com"
-    bucket                      = var.state_bucket_name
-    
+    endpoint = "https://sgp1.digitaloceanspaces.com"
+    bucket   = var.state_bucket_name
+
     # 2. Fixed: Matched exactly to the 'state_key' parameter in your workflow yaml file
-    key                         = "core/network.tfstate" 
-    
+    key = "core/network.tfstate"
+
     region                      = "us-east-1"
     skip_credentials_validation = true
     skip_metadata_api_check     = true
